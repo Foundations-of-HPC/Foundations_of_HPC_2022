d4fe490091e6b08dbf4e9f6c84403af24bc027e0
Luca Tornatore <gattoclochard@gmail.com>
2022-12-13 12:41:07 +0100

    examples for parallell debuggin

---
+++ Basic/AOB/debugging_parallel_examples/new 1.c
@@ -0,0 +1,28 @@
+void *ShowUp(void *thread_id)
+{
+  printf("Thread #%zd says: \" Hello World! \"\n", (size_t)thread_id);
+  pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[])
+{
+   pthread_t threads[NTHREADS];
+   int  rc;
+   size_t t;
+   
+   for(t=0; t<NTHREADS; t++)
+     {
+       
+       printf("Creating thread %zd\n", t);
+       rc = pthread_create(&threads[t], NULL, ShowUp, (void *)t);
+      
+       if (rc)
+	 {
+	   printf("return code from pthread_create() is %d\n", rc);
+	   exit(-1);
+	 }
+   }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+}
